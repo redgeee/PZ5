3478122ef521994a592d55ff4607df7b14bcad9f
is13101 <is13101@B206-4>
2023-02-22 08:33:10 +0700

    Добавьте файлы проекта.

---
+++ PZ5/Program.cs
@@ -0,0 +1,34 @@
+﻿Publisher publisher = new Publisher();
+ConcreteSubscribers s = new ConcreteSubscribers();
+publisher.Subscribe(s);
+
+
+class ConcreteSubscribers : ISubscriber
+{
+    public void update<T>(T context)
+    { }
+}
+
+interface ISubscriber
+{
+    public void update<T>(T context);
+}
+
+
+class Publisher
+{
+    private ISubscriber[] subscribers;
+    private int mainState;
+
+    public void Subscribe(ISubscriber s)
+    { }
+
+    public void Unsudscribe(ISubscriber s)
+    { }
+
+    public void NotifySubscribers()
+    { }
+
+    public void MainBusinessLogic()
+    { }
+}
